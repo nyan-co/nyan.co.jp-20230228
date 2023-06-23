894a972ae109936478e0e3279b5c75233f0a875c
yosh1 <yoppy1125yoppy@gmail.com>
2023-06-24 03:24:53 +0900

    Add OGP / favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,14 @@ const App = ({ Component, pageProps }: AppProps) => (
       <link rel="shortcut icon" href="/favicon.png" key="shortcutIcon" />
       <link rel="apple-touch-icon" href="/favicon.png" key="appleTouchIcon" />
       <meta name="theme-color" content="#000000" />
+      <meta name="description" content="にゃーん合同会社｜NYAN CO." />
       <link rel="icon" href="/favicon.png" />
+      {/* ogp */}
+      <title>
+        にゃーん合同会社｜NYAN CO.
+      </title>
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/ogp.png" />
       {/* <link rel="manifest" href="/manifest.json" /> */}
     </Head>
     <Component {...pageProps} />
